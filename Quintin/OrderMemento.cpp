197cae92f593e92bd2f043319d717406c5bdaaf8
copilotabuser <copilotabuser@github.com>
2023-11-01 03:25:25 +0200

    deleted some files

--- Quintin/OrderMemento.cpp
+++
@@ -1,7 +0,0 @@
-#include "OrderMemento.h"
-
-OrderMemento::OrderMemento(int orderId, std::string orderState) : orderId(orderId), orderState(orderState) {}
-
-std::string OrderMemento::getOrderState() const {
-    return orderState;
-}
