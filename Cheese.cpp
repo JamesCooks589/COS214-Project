e32e55e86442a2a801833644860c51fadfce17c6
SoMuchSalt <Oplayer589@gmail.com>
2023-11-02 20:37:55 +0200

    Updated comments to match ranges

--- Cheese.cpp
+++ Cheese.cpp
@@ -14,7 +14,7 @@ void Cheese::cookFood(vector<string> orderDetails, Plate* plate){
             //Seed current unix time to rand()
             int quality = rand() % 10 + 1;
             //If quality is 0...1, then the food is burnt so food(burnt)
-            //If quality is 4...6, then the food is fine so food(fine)
+            //If quality is 2...6, then the food is fine so food(fine)
             //If quality is 7...10, then the food is great so food(great)
             string product = "";
             if (quality <= 1)
