daf374b4f28508680e6fc47444cacf0545ff2a2a
zelinzzl <111583652+zelinzzl@users.noreply.github.com>
2023-11-03 13:16:12 +0200

    Modified CustomerGroup.h
    Added Split bill function to pay

---
+++ Code/CustomerGroup.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "CustomerComponent.h"
+#include <vector>
+
+class CustomerGroup : public CustomerComponent{
+    private:
+        std::vector<CustomerComponent*> customers;
+    public:
+        CustomerGroup(int id);
+        ~CustomerGroup();
+        void addToGroup(CustomerComponent* customer);
+        void givePlate(Plate* plate);
+        void setTableID(int id);
+        std::string printHappiness();
+        Order* getOrder();
+        int getSize();
+        void split(Bill* bill);
+};
